80c8dfdc90cde587f57fd3bb0a192df250429bcd
aarkwright <apollo.arkwright@gmail.com>
2019-01-22 11:36:44 +0200

    Set default request origin and referer.

--- flypy/settings.py
+++ flypy/settings.py
@@ -39,16 +39,19 @@ ROBOTSTXT_OBEY = True
 #TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
+DEFAULT_REQUEST_HEADERS = {
 #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 #   'Accept-Language': 'en',
-#}
+    'Content-Type': 'application/json',
+    'Origin': 'https://wizzair.com',
+    'Referer': 'https://wizzair.com',
+}
 
 # Enable or disable spider middlewares
 # See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
+# SPIDER_MIDDLEWARES = {
 #    'flypy.middlewares.FlypySpiderMiddleware': 543,
-#}
+# }
 
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
